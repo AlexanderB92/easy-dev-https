cedeb00823e07c625b0df102ae1ed215a68d41d7
AlexanderB92 <a.m.brandstrup@gmail.com>
2022-03-11 13:01:48 +0100

    port fix

--- server.js
+++ server.js
@@ -4,22 +4,19 @@ const fs = require('fs');
 //Put in env as usual
 const certPath = "files/selfsignedcert.pem";
 const keyPath = "files/selfsignedkey.pem";
-const passphrase = "<passphrase here>";
+const passphrase = "selfsign";
 const PORT = 5050;
 
 //Config
 const options = {
     key: fs.readFileSync(keyPath),
     cert: fs.readFileSync(certPath),
-    passphrase: passphrase,
-    PORT: PORT
+    passphrase: passphrase
 }
-
-
 
 https.createServer(options, (req,res) => {
     res.writeHead(200);
     res.end("https test");
-}).listen();
+}).listen(PORT);
 
 console.log(`Server started on ${PORT}`);
